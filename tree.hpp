f24e1fd2feebae322f9de0fbd689e4f1578e0762
David Medin <david@davidmedin.com>
2023-04-10 12:07:28 -0500

    first

---
+++ tree.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+class Branch {
+    
+};
+
+class Leaf {
+
+};
+
+class RTree {
+    list<Branch> branches;
+    list<Leaf> leaves;
+};
